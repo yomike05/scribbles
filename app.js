910b8e4a410b7d897213a131bd29cefc1fc640a8
MikeB <yomike05@gmail.com>
2012-07-30 16:11:42 -0700

    remove long polling block

--- app.js
+++ app.js
@@ -99,12 +99,6 @@ var server = http.createServer(function (request, response) {
 
 io = io.listen(server).set('log level', 1);
 
-// assuming io is the Socket.IO server object
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
-
 io.sockets.on('connection', function(socket) {
 
   socket.on('message', function(data) {
